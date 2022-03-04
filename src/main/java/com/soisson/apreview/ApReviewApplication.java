0d743d95d2f8e71dba9f7d5c3e3231db0b82abd2
sam <sam@example.com>
2022-03-03 19:21:15 -0600

    first

---
+++ src/main/java/com/soisson/apreview/ApReviewApplication.java
@@ -0,0 +1,13 @@
+package com.soisson.apreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApReviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soisson/apreview/ApReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soisson.apreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
