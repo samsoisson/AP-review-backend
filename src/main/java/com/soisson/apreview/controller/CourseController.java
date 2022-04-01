a557558a539e406e80701fbc2d74f42f773c953a
sam <sam@example.com>
2022-03-31 20:50:38 -0500

    postmeeting

--- src/main/java/com/soisson/apreview/controller/CourseController.java
+++ src/main/java/com/soisson/apreview/controller/CourseController.java
@@ -3,14 +3,16 @@ package com.soisson.apreview.controller;
 import com.soisson.apreview.model.Course;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 @RestController
 public class CourseController {
-    List<Course> courses = new ArrayList<>(Arrays.asList(new Course("Computer Science A","Easy","They don't usually teach, but the class is self-teachable.",
-            3.7,"A","less than 1 hour.")));
+    static Map<String, Course> courseMap = new HashMap();
+    static{
+        courseMap.put("compsci", new Course("Computer Science A","Easy","They don't usually teach, but the class is self-teachable.",
+                3.7,"A","less than 1 hour.","0"));
+        //add more courses
+    }
 //how to add more classes?
 
     //List<Course> courses = Arrays.asList(new Course("Computer Science A",4.0,5.1,3.7,93.3,0.5));
@@ -21,16 +23,17 @@ public class CourseController {
 //        return chemistry;
 //    }
     @CrossOrigin
-    @GetMapping("/courses/{index}")
-    public Course getCourse(@PathVariable int index)
+    @GetMapping("/courses/{courseId}")
+    public Course getCourse(@PathVariable String courseId)
     {
-        return courses.get(index);
+            return courseMap.get(courseId);
     }
 
+    @CrossOrigin
     @PostMapping("/courses")
     public Course addCourse(@RequestBody Course newCourse)
     {
-        courses.add(newCourse);
+        courseMap.putIfAbsent(newCourse.id, newCourse);
         return newCourse;
     }
 
--- src/main/java/com/soisson/apreview/model/Course.java
+++ src/main/java/com/soisson/apreview/model/Course.java
@@ -3,8 +3,8 @@ package com.soisson.apreview.model;
 public class Course {
     public String name,diff,teacher,grade,hw;
     public double ap;
-
-    public Course(String name,String diff,String teacher,double ap,String grade,String hw)
+    public String id;
+    public Course(String name,String diff,String teacher,double ap,String grade,String hw,String id)
     {
         this.name=name;
         this.diff=diff;
@@ -12,5 +12,6 @@ public class Course {
         this.ap=ap;
         this.grade=grade;
         this.hw=hw;
+        this.id=id;
     }
 }
