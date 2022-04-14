dea9f3d8e265dbd2108e1736ec4cf9edd0f6cf84
sam <sam@example.com>
2022-04-14 17:20:24 -0500

    4142022

--- src/main/java/com/soisson/apreview/controller/CourseController.java
+++ src/main/java/com/soisson/apreview/controller/CourseController.java
@@ -19,6 +19,50 @@ public class CourseController {
                 "No data","No data","No data"));
         courseMap.put("chemistry", new Course("Chemistry","No data","No data",
                 "No data","No data","No data"));
+        courseMap.put("chinese", new Course("Chinese","No data","No data",
+                "No data","No data","No data"));
+        courseMap.put("csa", new Course("Computer Science A","No data","No data",
+                "No data","No data","No data"));
+        courseMap.put("csp", new Course("Computer Science Principles","No data","No data",
+                "No data","No data","No data"));
+        courseMap.put("englishlang", new Course("English Language","No data","No data",
+                "No data","No data","No data"));
+        courseMap.put("englishlit", new Course("English Literature","No data","No data",
+                "No data","No data","No data"));
+        courseMap.put("environmental", new Course("Environmental Science","No data","No data",
+                "No data","No data","No data"));
+        courseMap.put("french", new Course("French","No data","No data",
+                "No data","No data","No data"));
+        courseMap.put("german", new Course("German","No data","No data",
+                "No data","No data","No data"));
+        courseMap.put("humangeo", new Course("Human Geography","No data","No data",
+                "No data","No data","No data"));
+        courseMap.put("latin", new Course("Latin","No data","No data",
+                "No data","No data","No data"));
+        courseMap.put("macro", new Course("Macroeconomics","No data","No data",
+                "No data","No data","No data"));
+        courseMap.put("micro", new Course("Microeconomics","No data","No data",
+                "No data","No data","No data"));
+        courseMap.put("musictheory", new Course("Music Theory","No data","No data",
+                "No data","No data","No data"));
+        courseMap.put("physics1", new Course("Physics 1","No data","No data",
+                "No data","No data","No data"));
+        courseMap.put("physics2", new Course("Physics 2","No data","No data",
+                "No data","No data","No data"));
+        courseMap.put("physicsc", new Course("Physics C","No data","No data",
+                "No data","No data","No data"));
+        courseMap.put("psych", new Course("Psychology","No data","No data",
+                "No data","No data","No data"));
+        courseMap.put("spanish", new Course("Spanish","No data","No data",
+                "No data","No data","No data"));
+        courseMap.put("stats", new Course("Statistics","No data","No data",
+                "No data","No data","No data"));
+        courseMap.put("usgov", new Course("US Government","No data","No data",
+                "No data","No data","No data"));
+        courseMap.put("ushistory", new Course("US History","No data","No data",
+                "No data","No data","No data"));
+        courseMap.put("world", new Course("World History","No data","No data",
+                "No data","No data","No data"));
         //getCourse("compsci");
         //System.out.println(courseMap.get("compsci"));
         //add more courses
